38e960ee33816e88b39890863663526c0039d9ee
Victor Javier Martinez Maldonado <kepy287@gmail.com>
2025-05-04 01:38:03 -0400

    corrigiendo el main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import 'dotenv/config'; // Importa dotenv para cargar las variables de entorno
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001>', // Reemplaza con el origen de tu frontend (ej: 3001, 3002)
+    origin: 'http://localhost:3001', // Reemplaza con el origen de tu frontend (ej: 3001, 3002)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // Si necesitas enviar cookies de sesión, etc.
     allowedHeaders: 'Content-Type, Authorization', // Lista de encabezados permitidos
