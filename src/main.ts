5cac7454e6295bc96a66397954e42d196ff1d85f
Victor Javier Martinez Maldonado <kepy287@gmail.com>
2025-05-04 01:23:39 -0400

    se modifico el main.ts en el backend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import 'dotenv/config'; // Importa dotenv para cargar las variables de entorno
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001>', // Reemplaza con el origen de tu frontend (ej: 3001, 3002)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Si necesitas enviar cookies de sesión, etc.
+    allowedHeaders: 'Content-Type, Authorization', // Lista de encabezados permitidos
+  });
   await app.listen(3000);
   //await app.listen(process.env.PORT ?? 3000);
 }
