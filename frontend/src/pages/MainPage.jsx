c777640c9261ce7a8b463889a254df4a89a5db0c
Victor Javier Martinez Maldonado <kepy287@gmail.com>
2025-05-05 23:17:23 -0400

    tst

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App'; // Importa tu componente principal App
-import './index.css'; // Importa estilos globales (opcional)
+ import 'src/index.css'; // Importa estilos globales (opcional)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+export default MainPage;
