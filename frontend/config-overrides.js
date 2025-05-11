a7e21d2ae7d39e858fcf6c7402bcef1907c4596e
Victor Javier Martinez Maldonado <kepy287@gmail.com>
2025-05-10 23:27:12 -0400

    modificando config-overrides.js

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,18 +2,39 @@ const { override } = require('customize-cra');
 
 module.exports = override(
   (config) => {
-    const svgRule = config.module.rules.find(rule => rule.test && rule.test.toString() === /\.svg$/.toString());
-
-    if (svgRule) {
-      svgRule.use = [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            throwIfNamespace: false,
-          },
-        },
-      ];
-    }
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf = rule.oneOf.map((oneOfRule) => {
+          if (
+            oneOfRule.test &&
+            oneOfRule.test.toString() === /\.svg$/.toString() &&
+            oneOfRule.loader === require.resolve('@svgr/webpack')
+          ) {
+            return {
+              ...oneOfRule,
+              options: {
+                ...oneOfRule.options,
+                throwIfNamespace: false,
+              },
+            };
+          }
+          return oneOfRule;
+        });
+      } else if (rule.test && rule.test.toString() === /\.svg$/.toString()) {
+        return {
+          ...rule,
+          use: [
+            {
+              loader: '@svgr/webpack',
+              options: {
+                throwIfNamespace: false,
+              },
+            },
+          ],
+        };
+      }
+      return rule;
+    });
 
     return config;
   }
