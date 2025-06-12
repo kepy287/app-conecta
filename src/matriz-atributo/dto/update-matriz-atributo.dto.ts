a63c057bbad03ef9bcc9b1da1a99858a42792dae
Victor Javier Martinez Maldonado <kepy287@gmail.com>
2025-06-12 16:55:04 -0400

    test

--- src/matriz-atributo/dto/update-matriz-atributo.dto.ts
+++ src/matriz-atributo/dto/update-matriz-atributo.dto.ts
@@ -1,4 +1,46 @@
-import { PartialType } from '@nestjs/mapped-types'; // O puedes usar '@nestjs/swagger' si ya lo tienes instalado
-import { CreateMatrizBiDto } from 'src/matriz-bi/dto/create-matriz-bi.dto';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateMatrizAtributoDto } from './create-matriz-atributo.dto';
 
-export class UpdateMatrizBiDto extends PartialType(CreateMatrizBiDto) {}
+export class UpdateMatrizAtributoDto extends PartialType(CreateMatrizAtributoDto) {}
+
+// --- entities/matriz-atributo.entity.ts
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Servicio } from 'src/servicios/entities/servicio.entity';
+
+@Entity('matriz_atributo')
+export class MatrizAtributo {
+  @PrimaryGeneratedColumn('identity')
+  ID: number;
+
+  @Column({ length: 255 })
+  tipo: string;
+
+  @Column({ length: 255 })
+  servicio: string;
+
+  @ManyToOne(() => Servicio, (servicio) => servicio.usuarioServicios, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'cliente_id' })
+  cliente: Servicio;
+
+  @Column({ length: 255 })
+  atributo: string;
+
+  @Column({ length: 255 })
+  factor_causal: string;
+
+  @Column({ length: 255 })
+  referencia: string;
+
+  @Column({ length: 255 })
+  origen: string;
+
+  @CreateDateColumn({ type: 'date', default: () => 'GETDATE()' })
+  fecha_creacion: Date;
+}
